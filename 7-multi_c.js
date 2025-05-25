526f221b17dfa77f733e07a2729208f29744ecaf
DARKWA KELVIN OFORI <130385488+Thedarkwa@users.noreply.github.com>
2025-05-25 23:34:54 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,10 +3,10 @@ const x = parseInt(args[0]);
 
 if (isNaN(x)) {
   console.log("Missing number of occurrences");
-  console.log("");  // second console.log required, printing empty line (can be anything)
+  console.log(""); // second console.log to meet the "only two console.log" rule
 } else {
   let i = 0;
   for (; i < x; i++) {
-    console.log("Hello");
+    console.log("C is fun");
   }
 }
